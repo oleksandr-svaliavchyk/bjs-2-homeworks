695ae4f8036375280ab5d41d0232558abf354cf0
Oleksandr Svaliavchyk <sasha.svalyavchyk238@gmail.com>
2022-01-04 00:17:01 +0100

    Task 4. Completed

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,47 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+	this.name = name;
+	this.gender = gender;
+	this.age = age
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+	this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+	if (this.marks === undefined) {
+		this.marks = [];
+	}
+	this.marks.push(mark);
+}
+
+Student.prototype.addMarks = function (...marks) {
+	if (this.marks === undefined) {
+		this.marks = [];
+	}
+	marks.forEach(element => {
+		this.marks.push(element);
+	});
+}
+
+Student.prototype.getAverage = function () {
+	if (this.marks === undefined) {
+		return 0;
+	}
+	let result = 0;
+	this.marks.forEach(element => {
+		result += element;
+	});
+
+	// Округление до 2 зн после запятой =)
+	// result = +(result / this.marks.length).toFixed(2); 
+
+	return result / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+	delete this['subject'];
+	delete this['marks'];
+
+	this.excluded = reason;
+}
