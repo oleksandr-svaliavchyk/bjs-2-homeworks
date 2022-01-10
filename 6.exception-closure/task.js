cd95104181e1fb5121df8b514bfe956afa51e470
Oleksandr Svaliavchyk <sasha.svalyavchyk238@gmail.com>
2022-01-10 22:43:56 +0100

    Task 6 Completed

---
+++ 6.exception-closure/task.js
@@ -0,0 +1,55 @@
+
+function parseCount(num) {
+	num = Number.parseInt(num);
+	if (Number.isNaN(num)) {
+		throw new Error('Невалидное значение');
+	}
+	return num;
+}
+
+function validateCount(num) {
+	try {
+		num = parseCount(num);
+	} catch (Error) {
+		return Error;
+	}
+	return num;
+}
+
+class Triangle {
+	constructor(a, b, c) {
+		if ((a + b < c) || (a + c < b) || (b + c < a)) {
+			throw new Error('Треугольник с такими сторонами не существует');
+		}
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+
+	getPerimeter() {
+		return this.a + this.b + this.c;
+	}
+
+	getArea() {
+		let p = 1 / 2 * (this.getPerimeter());
+		return (+(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3)));
+	}
+
+}
+
+function getTriangle(a, b, c) {
+	try {
+		let t = new Triangle(a, b, c)
+		return t;
+	} catch (Error) {
+		let obj = {
+			getArea() {
+				return 'Ошибка! Треугольник не существует';
+			},
+			getPerimeter() {
+				return 'Ошибка! Треугольник не существует';
+			}
+		}
+		return obj;
+	}
+}
