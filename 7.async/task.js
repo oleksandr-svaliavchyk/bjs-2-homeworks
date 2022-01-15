514f07ac22ea469eaaf1619164b07c8dc60a973b
Oleksandr Svaliavchyk <sasha.svalyavchyk238@gmail.com>
2022-01-15 21:01:54 +0100

    Task 7 Completed

---
+++ 7.async/task.js
@@ -0,0 +1,84 @@
+class AlarmClock {
+	constructor() {
+		this.alarmCollection = [];
+		this.timerId = null;
+	}
+
+	addClock(time, callback, id) {
+
+		if (!Number.isInteger(id)) {
+			throw new Error('Не правильный айди');
+		}
+
+		if (this.alarmCollection.some(element => {
+			if (element.id === id) {
+				return true;
+			}
+		})) {
+			console.error('Такой айди звонка уже существует ');
+		} else {
+			this.alarmCollection.push({
+				id,
+				time,
+				callback,
+			});
+		}
+	}
+
+	removeClock(id) {
+		this.alarmCollection.filter((element, idx) => {
+			if (element.id === id) {
+				this.alarmCollection.splice(idx, 1);
+				return 1;
+			}
+		});
+		return 0;
+	}
+
+	getCurrentFormattedTime() {
+		let date = new Date;
+
+		let hours = date.getHours();
+		let minutes = date.getMinutes();
+		if (String(minutes).length === 1) {
+			minutes = '0' + minutes;
+		}
+		if (String(hours).length === 1) {
+			hours = '0' + hours;
+		}
+
+		return (`${hours}:${minutes}`);
+	}
+
+	checkClock(alarmСlock) {
+		if (alarmСlock.time === this.getCurrentFormattedTime()) {
+			alarmСlock.callback();
+		}
+	}
+
+	start() {
+		this.timerId = setInterval(() => {
+			this.alarmCollection.forEach(element => {
+				this.checkClock(element);
+			});
+		});
+	}
+
+	stop() {
+		if (this.timerId) {
+			clearInterval(this.timerId);
+			this.timerId = null;
+		}
+	}
+
+	printAlarms() {
+		this.alarmCollection.forEach(element => {
+			console.log(`${element.id} ${element.time}`)
+		});
+	}
+
+	clearAlarms() {
+		clearInterval(this.timerId);
+		this.alarmCollection.length = 0;
+	}
+}
